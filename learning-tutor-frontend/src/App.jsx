25dc994bd09cc599279a2bd06050d909cb9ac293
AasishDairelSahayaGrinspan <aasishdairel@gmail.com>
2026-02-07 10:12:16 +0530

    fix: layout constraints and editor shrinking

--- learning-tutor-frontend/src/App.jsx
+++ learning-tutor-frontend/src/App.jsx
@@ -1221,7 +1221,7 @@ function App() {
             </div>
           </div>
 
-          <div className="editor-wrapper" style={{ flex: 1 }}>
+          <div className="editor-wrapper" style={{ flex: 1, minHeight: 0 }}>
             <Editor
               height="100%"
               language={LANGUAGES.find(l => l.value === language)?.monacoId}
